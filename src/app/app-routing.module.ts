2eaa383fc173c207a4f6fa485a3fc1d7966eeba3
yaakoubichaima <chaimayaakoubi50@gmail.com>
2023-02-25 21:58:15 +0100

    add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { AppComponent } from './app.component';
 import { BlogDetailsComponent } from './blog-details/blog-details.component';
 import { BlogFormComponent } from './blog-form/blog-form.component';
 import { BlogsListPageComponent } from './blogs-list-page/blogs-list-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  // { path: '', component: AppComponent },
+  { path: '', component: HomePageComponent },
   { path: 'preview', component: BlogsListPageComponent },
   { path: 'new', component: BlogFormComponent },
   { path: 'details/:id', component: BlogDetailsComponent }
