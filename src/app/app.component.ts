4259a18996c50c30950445dc3ea4d88ed9693bb6
yaakoubichaima <chaimayaakoubi50@gmail.com>
2023-02-27 19:04:26 +0100

    change app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<app-navbar></app-navbar>
-    <div style="margin-top: 50px;"><router-outlet></router-outlet></div>`,
+    <div style="margin: 3% 10% 5% 10% ;"><router-outlet></router-outlet></div>`,
 })
 export class AppComponent {
   title = 'blog-app';
