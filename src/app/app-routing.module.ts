7b79360827c1c0599db26cb1244833ab85f55440
yaakoubichaima <chaimayaakoubi50@gmail.com>
2023-02-27 17:27:28 +0100

    update imports for the app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { BlogDetailsComponent } from './blog-details/blog-details.component';
-import { BlogFormComponent } from './blog-form/blog-form.component';
-import { BlogsListPageComponent } from './blogs-list-page/blogs-list-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { BlogDetailsComponent } from './components/blog-details/blog-details.component';
+import { BlogFormComponent } from './components/blog-details/blog-form/blog-form.component';
+import { BlogsListPageComponent } from './components/blogs-list-page/blogs-list-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
